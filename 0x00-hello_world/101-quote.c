21bac1a9d2b55fe323cc6715dd6b08a50cb40601
MwauraHellen <helenemwaura@gmail.com>
2023-03-19 03:35:06 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+/**
+ * main - main function
+ *Return: 1 is error
+ */
 
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const ssize_t length = sizeof(message) - 1; // exclude null terminator
+	const ssize_t length = sizeof(message) - 1; 
 	write(STDERR_FILENO, message, length);
 	return (1);
 }
